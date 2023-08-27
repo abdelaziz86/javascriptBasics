b0b679e5deca57c978003f11aa49c68654c6045c
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-27 15:53:10 +0200

    event listener

--- script.js
+++ script.js
@@ -1,34 +1,72 @@
 var myName = "John Doe";
 
-console.log(myName);
+// console.log(myName);
 
 myName = 25;  
 
-console.log(myName); 
+// console.log(myName); 
 
 // ============ conditions ============
 
 var score = 80; 
 if (score >= 70) {
-    console.log("Exam Passed"); 
+    // console.log("Exam Passed"); 
 } else {
-    console.log("Exam Failed");     
+    // console.log("Exam Failed");     
 }
 
 
 // ========= loops ==========
 for (var i = 0; i < 10; i++) {
-    console.log(i); 
+    // console.log(i); 
 }
 
 
 // =========== Array ===========
 var names = ["John", 18, "Jane", 25, "Jim"];
-console.log(names[1]); 
+// console.log(names[1]); 
+
+// ============= OBJECT =============
+var car = {
+    type: "Fiat",
+    color: "Black",
+    Speed: 200
+}; 
+// console.log(car.type); 
+
+// ============= Function =============
+function acceleration(obj) { 
+    return obj.Speed * 5;
+}
+
+// console.log(acceleration(car)); 
 
 
 
+// =========== ACCESSING HTML ELEMENTS ============
+// var h1 = document.getElementById("hello") ; 
+// console.log("NO EVENT LISTENER : " + h1.textContent);
 
+
+// ===== Event listeners ===========
+document.addEventListener("DOMContentLoaded", function () {
+    // var h1 = document.getElementById("hello");
+    // console.log("WITH EVENT LSITENER : " + h1.textContent);
+});
+
+//=========================================
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+    // var h1 = document.getElementById("hello");
+    // h1.textContent = "Goodbye";
+
+    document.getElementById("hello").textContent = "Goodbye";
+
+
+
+    // document.getElementById("hello") = "goodbye"; 
+    // h1.textContent = "Goodbye"; 
+})
 
 
 
