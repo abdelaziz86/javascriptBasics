2d54f64770861f299f9f97afd76bd274fa718ffd
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-27 14:04:28 +0200

    ARRAY

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ myName = 25;
 
 console.log(myName); 
 
+// ============ conditions ============
+
+var score = 80; 
+if (score >= 70) {
+    console.log("Exam Passed"); 
+} else {
+    console.log("Exam Failed");     
+}
+
+
+// ========= loops ==========
+for (var i = 0; i < 10; i++) {
+    console.log(i); 
+}
+
+
+// =========== Array ===========
+var names = ["John", 18, "Jane", 25, "Jim"];
+console.log(names[1]); 
+
+
+
+
+
+
+
